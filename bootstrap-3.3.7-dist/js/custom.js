af03da545b1d75359197a7eb3e9009c178c803ec
Anekha Sokhal <Anekha@anekhas-air.mynet>
2016-11-05 16:09:59 +0000

    added same sort of header to the about us page and corrected the path for the image

--- bootstrap-3.3.7-dist/js/custom.js
+++ bootstrap-3.3.7-dist/js/custom.js
@@ -7,3 +7,15 @@ $(window).scroll(function() {
         $('.header_one').removeClass("sticky");
     }
 });
+
+$(document).ready(function(){
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 65;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('.navbar-default').addClass('fixed');
+			 }
+			 else {
+				 $('.navbar-default').removeClass('fixed');
+			 }
+		});
+	});
