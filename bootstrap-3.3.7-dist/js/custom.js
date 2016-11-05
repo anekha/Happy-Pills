ae69c2c2f870761cf2439975d763995bd1df8b0b
Anekha Sokhal <Anekha@anekhas-air.mynet>
2016-11-05 21:32:29 +0000

    cleaning up

--- bootstrap-3.3.7-dist/js/custom.js
+++ bootstrap-3.3.7-dist/js/custom.js
@@ -1,21 +1,36 @@
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('.header_one').addClass("sticky");
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#content-anchor').offset().top;
+    if (window_top > div_top) {
+        $('.header_one').addClass('sticky');
+        $('#sticky-phantom').show();
+    } else {
+        $('.header_one').removeClass('sticky');
+        $('#sticky-phantom').hide();
     }
-    else{
-        $('.header_one').removeClass("sticky");
-    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
 });
 
-$(document).ready(function(){
-	   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() > 10;
-			 if ($(window).scrollTop() > navHeight) {
-				 $('nav').addClass('fixed');
-			 }
-			 else {
-				 $('nav').removeClass('fixed');
-			 }
-		});
-	});
+
+
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  xs
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 20) {
+      $('nav').addClass('fixed');
+    }
+    if ($(window).scrollTop() < 21) {
+      $('nav').removeClass('fixed');
+    }
+  });
+});
